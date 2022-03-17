c19222e09626aaf5fa09daef30c04c87d62dbfb0
Dmitry <di.mesler.2010@yandex.ru>
2022-03-17 17:54:09 +0300

    Chapter 9, Classes - Refactoring: task 9.14

--- chapter_9/fourteen.py
+++ chapter_9/fourteen.py
@@ -5,5 +5,4 @@ from random import choice
 numbers = list(range(10))
 letters = [choice(string.ascii_lowercase.upper()) for _ in range(4)]
 list_ = numbers + letters
-for _ in range(4):
-    print(choice(list_), end='')
+print(*[choice(list_) for _ in range(4)], sep='')
