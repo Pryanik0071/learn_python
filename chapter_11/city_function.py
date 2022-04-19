b21f391da2f3fcad8520f7acbc2b200162f4671e
Dmitry <di.mesler.2010@yandex.ru>
2022-04-19 16:36:25 +0300

    Chapter 11, Unit tests - Done: task 11.2

--- chapter_11/city_function.py
+++ chapter_11/city_function.py
@@ -1,5 +1,12 @@
-"""Task - 11.1. Город, страна"""
+"""Task - 11.1. Город, страна
+    Task - 11.2. Население"""
 
 
-def country_city(country, city):
-    return f'<<{country}, {city}>>'.title()
+def country_city(country: str, city: str, population: int = None) -> str:
+    """Возвращает Страну + Имя города + население в формате:
+        :return <<Country, City - population number>>
+    """
+    country, city = (_.capitalize() for _ in (country, city))
+    if population:
+        return f'<<{country}, {city} - population {population}>>'
+    return f'<<{country}, {city}>>'
--- chapter_11/test_cities.py
+++ chapter_11/test_cities.py
@@ -5,8 +5,14 @@ from chapter_11.city_function import country_city
 
 class TestCity(unittest.TestCase):
     def test_city_country(self):
+        """Тест без населения"""
         self.assertEqual(country_city('santiago', 'chile'),
                          '<<Santiago, Chile>>')
+
+    def test_city_country_population(self):
+        """Тест с населением"""
+        self.assertEqual(country_city('santiago', 'chile', population=5_000_000),
+                         '<<Santiago, Chile - population 5000000>>')
 
 
 if __name__ == '__main__':
